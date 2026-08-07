2c35869e5f0c8a0001b204b972ebf38fdb440832
Hinali Chodavadiya <hinali7766@gmail.com>
2026-08-07 20:50:47 +0530

    Display the menu data

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ console.log("CafeFlow Started");
 //! for form feature-add
 const menuForm = document.getElementById("menuForm");
 
+let menuItems = JSON.parse(localStorage.getItem("menuItems")) || [];
+
 menuForm.addEventListener("submit", function (e) {
 
     e.preventDefault();
@@ -15,10 +17,36 @@ menuForm.addEventListener("submit", function (e) {
         price: document.getElementById("price").value
     };
 
-    console.log(menuItem);
+    menuItems.push(menuItem);
 
-    alert("Menu Item Added Successfully!");
+    localStorage.setItem("menuItems", JSON.stringify(menuItems));
+
+    displayMenuItems();
 
     menuForm.reset();
 
 });
+
+
+//! display menu-itmes
+const menuTable = document.getElementById("menuTable");
+let menuItems = JSON.parse(localStorage.getItem("menuItems")) || [];
+function displayMenuItems() {
+    menuTable.innerHTML = "";
+    menuItems.forEach((item,index)=>{
+        menuTable.innerHTML += `
+            <tr>
+                <td>${item.id}</td>
+                <td>${item.name}</td>
+                <td>${item.category}</td>
+                <td>₹${item.price}</td>
+                <td>
+                    <button class="delete-btn">
+                        Delete
+                    </button>
+                </td>
+            </tr>
+        `;
+    });
+}
+displayMenuItems();

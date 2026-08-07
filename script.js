87cf3ee295d5472eecffa6a88fdbb5ea17a8e114
Hinali Chodavadiya <hinali7766@gmail.com>
2026-08-07 22:53:56 +0530

    Search feature

--- script.js
+++ script.js
@@ -39,3 +39,51 @@ function displayMenuItems() {
 displayMenuItems();
 
 //! display menu items
+
+
+
+//! search
+const searchInput = document.getElementById("searchInput");
+const searchBtn = document.getElementById("searchBtn");
+const resetBtn = document.getElementById("resetBtn");
+
+searchBtn.addEventListener("click", searchMenuItem);
+
+resetBtn.addEventListener("click", () => {
+    searchInput.value = "";
+    displayMenuItems();
+});
+
+function searchMenuItem(){
+
+    const id = searchInput.value.trim();
+
+    const filteredItems = menuItems.filter(item => item.id === id);
+
+    menuTable.innerHTML = "";
+
+    filteredItems.forEach((item,index)=>{
+
+        menuTable.innerHTML += `
+        <tr>
+
+            <td>${item.id}</td>
+
+            <td>${item.name}</td>
+
+            <td>${item.category}</td>
+
+            <td>₹${item.price}</td>
+
+            <td>
+                <button class="delete-btn">
+                    Delete
+                </button>
+            </td>
+
+        </tr>
+        `;
+
+    });
+
+}

c936c474c6cc97daa4ef0d3736cd10cd0758e340
Hinali Chodavadiya <hinali7766@gmail.com>
2026-08-07 15:12:12 +0530

    Added Menu Item functionality

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log("CafeFlow Started");
+
+
+//! for form feature-add
+const menuForm = document.getElementById("menuForm");
+
+menuForm.addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    const menuItem = {
+        id: document.getElementById("itemId").value,
+        name: document.getElementById("itemName").value,
+        category: document.getElementById("category").value,
+        price: document.getElementById("price").value
+    };
+
+    console.log(menuItem);
+
+    alert("Menu Item Added Successfully!");
+
+    menuForm.reset();
+
+});
